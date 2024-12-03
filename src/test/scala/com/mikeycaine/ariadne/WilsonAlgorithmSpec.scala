d29269ec7f4979394dcc6f7300b0ac57e49590e1
Mike Caine <mikey.caine@btinternet.com>
2024-12-03 13:24:48 +0000

    Dijkstra, write png

--- src/test/scala/com/mikeycaine/ariadne/WilsonAlgorithmSpec.scala
+++ src/test/scala/com/mikeycaine/ariadne/WilsonAlgorithmSpec.scala
@@ -9,7 +9,7 @@ import scala.util.{Try, Success, Failure}
 
 class WilsonAlgorithmSpec extends AnyFlatSpec with Matchers {
 
-  def checkTry[F](t: Try[F]) = t match {
+  def checkTry[F](t: Try[F]): Unit = t match {
     case Success(_) =>
     case Failure(exception: Exception) => fail(exception.getMessage)
   }
@@ -25,12 +25,11 @@ class WilsonAlgorithmSpec extends AnyFlatSpec with Matchers {
     val d = dijkstra.distances(0, 0)
     val maxDist = d.values.max
     val colours = d map {
-      case (cell: Cell, distance) => {
+      case (cell: Cell, distance) =>
         val c = Math.max(0, Math.min(255, 255 - 255 * distance / maxDist))
         val d = 255 - c
         val colour = new Color(c, 120, 120)
         ((cell.row, cell.col) -> colour)
-      }
     }
 
     checkTry(Grid2Png.writeWithColours(maze, outputFile, colours))
