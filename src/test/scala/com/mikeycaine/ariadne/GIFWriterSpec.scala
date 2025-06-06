69a6f0867d137801ebbba520da1fe53818bb4a54
Mike Caine <mikey.caine@btinternet.com>
2025-06-06 13:24:07 +0100

    Don't change distance colours in GIFWriterSpec

--- src/test/scala/com/mikeycaine/ariadne/GIFWriterSpec.scala
+++ src/test/scala/com/mikeycaine/ariadne/GIFWriterSpec.scala
@@ -8,7 +8,6 @@ import java.awt.image.BufferedImage
 class GIFWriterSpec extends AriadneBaseSpec {
   "GIFWriter" should "write a GIF" in {
     val size = 50
-
     val maze: GridMaze = HunterKillerAlgorithm(size, size)
 
     val distances: Map[Cell, Int] = Dijkstra(maze).distances(0, 0)
@@ -20,14 +19,15 @@ class GIFWriterSpec extends AriadneBaseSpec {
 
     val nFrames = pathLength
 
+    val dijkstraColours = GridMaze.distanceColours(distances)
+
     val frameGenerator: Int => BufferedImage = (i: Int) => {
-      val dijkstraColours = GridMaze.distanceColours(distances.filter((k, v) => v < i))
       val routeColours = path.take(i).map(cell => ((cell.row, cell.col) -> Color.RED)).toMap
       bufferedImageForGrid(maze, dijkstraColours ++ routeColours)
     }
 
     val result = GIFWriter.writeGIF("xxx.gif", frameGenerator, nFrames)
     assert(result.isSuccess)
-    result.map(p => println(s"Created $p"))
+    //result.map(p => println(s"Created $p"))
   }
 }
